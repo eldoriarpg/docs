f733d56d09c964dffafd5a32eebd814e264aa82e
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2025-02-20 10:45:33 +0100

    Feature/update (#18)
    * Define third party repos for gradle
    
    * Update pipfile.lock

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,35 +7,4 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    // External repository
-    maven("https://papermc.io/repo/repository/maven-public/")
-}
-
-dependencies {
-    // We are not responsible to provide the classes from this dependency
-    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    // We are responsible for providing the classes from this dependency
-    implementation("de.chojo.sadu", "sadu", "2.3.1")
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-    withSourcesJar()
-    withJavadocJar()
-}
-
-tasks {
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-
-    compileTestJava {
-        options.encoding = "UTF-8"
-    }
-
-    javadoc {
-        options.encoding = "UTF-8"
-    }
 }
